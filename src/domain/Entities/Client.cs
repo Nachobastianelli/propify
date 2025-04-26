c1563d79f2e20777a73275c7530019dce4dc3e6e
Nachobastianelli <nachobastianelli2003@gmail.com>
2025-04-25 21:25:30 -0300

    Carga inicial de la entidad usuarios

---
+++ src/domain/Entities/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain.Entities
+{
+    public class Client : User
+    {
+        public Client() { }
+        public Client(string name, string surname, string email, string password, string numberphone, Enum documentType, string dni)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Password = password;
+            NumberPhone = numberphone;
+            DocumentType = documentType;
+            Dni = dni;
+        }
+    }
+
+    
+}
