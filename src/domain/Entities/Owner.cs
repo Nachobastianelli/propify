15890709751b273831e6804eb3ce24c26f894141
JulianPaquez <julianpaquez@gmail.com>
2025-04-26 14:42:50 -0300

    Creacion de la entidad Owner y Property

--- src/domain/Entities/Owner.cs
+++ src/domain/Entities/Owner.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace domain.Entities
 {
-    internal class Owner
-    {
+    public class Owner : User
+    { 
+        public int PropertyId { get; set; }
+        public Property Property { get; set; }
+
+        public int Cvu {  get; set; }
+
+        public Owner() { }
+
+        public Owner(string name, string surname, string email, string password, string numberphone, Enum documentType, string dni,int propertyId, int cvu) 
+        { 
+            Name = name;
+            Surname = surname;
+            Email = email;  
+            Password = password;
+            NumberPhone = numberphone;
+            DocumentType = documentType;
+            Dni = dni;
+            PropertyId = propertyId;
+            
+            Cvu = cvu;
+        }
     }
 }
